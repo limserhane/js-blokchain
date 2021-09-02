f20ba427064eaf1e34f5654a8750d7d882e0665b
limserhane <limserhane@protonmail.com>
2021-09-02 22:08:52 +0200

    fix add ip

--- add.sh
+++ add.sh
@@ -3,14 +3,5 @@
 # $1 - filename
 # $2 - hash
 
-
-if [ -z "$3" ]
-then
-    ip="localhost"
-else
-    ip=$3
-fi
-
-
 curl -H "Content-type:application/json" --data '{"data" : "'$1'/'$2'"}' http://localhost:3001/mineBlock
 
